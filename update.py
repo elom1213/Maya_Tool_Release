cebef0c8e30fdd55558e174b3cd6236cd5e15da0
JunnyPark <junny.park1213@gmail.com>
2026-05-10 17:29:12 +0900

    update force override when update

--- update.py
+++ update.py
@@ -3,14 +3,33 @@ import os
 
 ROOT = os.path.dirname(__file__)
 
-print("Pulling latest version...")
 
-result = subprocess.run(
-    ["git", "pull"],
-    cwd=ROOT,
-    capture_output=True,
-    text=True
-)
+def run_git(cmd):
 
-print(result.stdout)
-print(result.stderr)
+    print(f"\n>>> {' '.join(cmd)}")
+
+    result = subprocess.run(
+        cmd,
+        cwd=ROOT,
+        capture_output=True,
+        text=True,
+        shell=True
+    )
+
+    print(result.stdout)
+
+    if result.stderr:
+        print(result.stderr)
+
+
+# 최신 정보 fetch
+run_git(["git", "fetch", "--all"])
+
+# 로컬 변경 강제 제거
+run_git(["git", "reset", "--hard", "origin/master"])
+
+# 추적 안 되는 파일 제거
+run_git(["git", "clean", "-fd"])
+
+
+print("\nUpdate Complete")
